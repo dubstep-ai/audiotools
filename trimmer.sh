588357dc723d2d3645b6aa9c8a95c6bd0f457347
Arvind Suresh <arvind.suresh@gmail.com>
2021-08-18 12:09:07 -0400

    modify trimmer for latam dataset

--- trimmer.sh
+++ trimmer.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # Trim the beginning and the end of the audio file for silences
 FILES='*'
-for f in $FILES
+for f in $(find ~/data/LatinAmericanTTS -name "22050_*.wav")
 do
-  echo "Processing $f file..."
+  dirname=$(dirname "$f")
+  filename=$(basename "$f")
   # take action on each file. $f store current file name
-  sox $f temp.wav silence 1 0.07 1% reverse
-  sox temp.wav ./trimmed/$f silence 1 0.07 1% reverse 
+  sox $dirname/$filename temp.wav silence 1 0.07 1% reverse
+  sox temp.wav $dirname/trimmed_$filename silence 1 0.09 1% reverse 
+  #rm $dirname/trimmed_$filename
 done
 
